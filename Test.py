0da9f16516f304fb9662620854d80ce6791b3268
HsiaoYetGun <hsiaoyetgun@gmail.com>
2018-08-03 10:47:03 +0800

    batch predict

--- Test.py
+++ Test.py
@@ -1,7 +1,6 @@
 '''
 Created on July 20, 2018
 @author : hsiaoyetgun (yqxiao)
-Reference : Enhanced LSTM for Natural Language Inference (ACL 2017)
 '''
 # coding: utf-8
 from __future__ import absolute_import
@@ -13,7 +12,7 @@ from Utils import *
 import tensorflow as tf
 import numpy as np
 import sys
-from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.metrics import f1_score
 from Model import ESIM
 import Config
 
@@ -24,38 +23,44 @@ def predict():
     start_time = time.time()
     premise_test, premise_mask_test, hypothesis_test, hypothesis_mask_test, y_test = sentence2Index(
         arg.testset_path, vocab_dict)
+    batches = next_batch(premise_test, premise_mask_test, hypothesis_test, hypothesis_mask_test, y_test, shuffle=False)
     time_diff = get_time_diff(start_time)
     print_log('Time usage : ', time_diff, file=log)
 
     with tf.Session() as sess:
         # load model
-        print("Loading model: " + arg.model)
-        saver = tf.train.import_meta_graph(arg.model + '.meta')
+        print("Loading model ...")
+        saver = tf.train.Saver(max_to_keep=5)
         saver.restore(sess, arg.best_path)
 
         # testing
         print_log('Start testing ...', file=log)
         start_time = time.time()
-        feed_dict = {model.premise: premise_test,
-                     model.premise_mask: premise_mask_test,
-                     model.hypothesis: hypothesis_test,
-                     model.hypothesis_mask: hypothesis_mask_test,
-                     model.dropout_keep_prob: 1.0}
-        logits, loss, acc = sess.run([model.logits, model.loss, model.acc], feed_dict = feed_dict)
-
+        y_pred = []
+        for batch in batches:
+            batch_premise_test, batch_premise_mask_test, batch_hypothesis_test, batch_hypothesis_mask_test, _ = batch
+            feed_dict = {model.premise: batch_premise_test,
+                         model.premise_mask: batch_premise_mask_test,
+                         model.hypothesis: batch_hypothesis_test,
+                         model.hypothesis_mask: batch_hypothesis_mask_test,
+                         model.dropout_keep_prob: 1.0}
+            logits = sess.run([model.logits], feed_dict = feed_dict)
+            logits = np.array(logits)
+            logits = logits.reshape([-1, logits.shape[-1]])
+            y_pred.extend(logits)
         # evaluating
-        y_pred = np.argmax(logits, 1)
+        y_pred = np.argmax(y_pred, 1)
         y_true = np.argmax(y_test, 1)
         f1 = f1_score(y_true, y_pred, average='weighted', labels=np.unique(y_true))
-        auc = roc_auc_score(y_true, y_pred, average='weighted')
+        acc = np.mean(y_true == y_pred)
         for id in range(len(y_true)):
-            print('Wrong classification:')
             if y_true[id] != y_pred[id]:
-                premise_text = ''.join([index2word[idx] for idx in premise_test[id]])
-                hypothesis_text = ''.join([index2word[idx] for idx in hypothesis_test[id]])
-                print('Left_text: {0} / Right_text: {1}'.format(premise_text, hypothesis_text))
-                print('The true label is {0} / The pred label is {1}'.format(y_true[id], y_pred[id]))
-        print('The test accuracy / f1 / auc: {0[0]:.2%} / {0[1]:.4f} / {0[2]:.4f}'.format((acc, f1, auc)))
+                premise_text = ''.join([index2word[idx] + ' ' for idx in premise_test[id] if index2word[idx] != '<PAD>'])
+                hypothesis_text = ''.join([index2word[idx] + ' ' for idx in hypothesis_test[id] if index2word[idx] != '<PAD>'])
+                print('Left_text: {0}/ Right_text: {1}'.format(premise_text, hypothesis_text))
+                print('The true label is {0}/ The pred label is {1}'.format(y_true[id], y_pred[id]))
+        print('The test accuracy: {0:>6.2%}'.format(acc))
+        print('The test F1: {0:>6.4}'.format(f1))
         time_diff = get_time_diff(start_time)
         print('Time usage: ', time_diff, '\n')
 
@@ -66,7 +71,7 @@ if __name__ == '__main__':
 
     vocab_dict = load_vocab(arg.vocab_path)
     arg.vocab_dict_size = len(vocab_dict)
-    index2word = {index : word for word, index in vocab_dict}
+    index2word = {index : word for word, index in vocab_dict.items()}
 
     if arg.embedding_path:
         embeddings = load_embeddings(arg.embedding_path, vocab_dict)
